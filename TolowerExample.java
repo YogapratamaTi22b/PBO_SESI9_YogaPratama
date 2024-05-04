02f2f245d34f0c3c83abec1253476f9031beb05e
YogapratamaTi22b <165447544+YogapratamaTi22b@users.noreply.github.com>
2024-05-04 19:45:50 +0700

    TolowerExample.java

---
+++ TolowerExample.java
@@ -0,0 +1,9 @@
+public class TolowerExample {
+   public static void main(String[] args) {
+    String stringOri = "Welcome to ngoding";
+    String stringLower = stringOri.toLowerCase();
+    System.out.println("Before using the tolowerCose methode: " + stringOri);
+    System.out.println(("After using the tolowerCose methode: " + stringLower));
+    
+   } 
+}
