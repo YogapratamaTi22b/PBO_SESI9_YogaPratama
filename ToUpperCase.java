28a210de85c7a4b02a54554f4b23c67ec642e42d
YogapratamaTi22b <165447544+YogapratamaTi22b@users.noreply.github.com>
2024-05-04 19:45:22 +0700

    ToUpperCase.java

---
+++ ToUpperCase.java
@@ -0,0 +1,9 @@
+public class ToUpperCase {
+    public static void main(String[] args) {
+        String stringOri = "Walcome to Mangcodig";
+        String stringUpper = stringOri.toUpperCase();
+        System.out.println("Before using the toUpperCase method: " + stringOri);
+        System.out.println("After using the toUpperCase method: " + stringUpper);
+         
+    }
+}
