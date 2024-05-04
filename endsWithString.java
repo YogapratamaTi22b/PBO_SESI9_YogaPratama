5a5d3c5690d39ba05afb06ff13355d3a074f7fbc
YogapratamaTi22b <165447544+YogapratamaTi22b@users.noreply.github.com>
2024-05-04 19:46:15 +0700

    endsWithString.java

---
+++ endsWithString.java
@@ -0,0 +1,10 @@
+public class endsWithString {
+
+    public static void main(String[] args) {
+        String string1 = "Hello how are you";
+        System.out.println("the result is: " + string1.endsWith("u"));
+        System.out.println("the result is: " + string1.endsWith("o"));
+        System.out.println("the result is: " + string1.endsWith("you"));
+        System.out.println("the result is: " + string1.endsWith("hou"));
+    }
+ }
